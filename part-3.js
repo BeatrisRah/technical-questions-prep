32345313ff1f7c7dcece4b8facb90a0c7fcf6296
Beatris <beatrisraj7@gmail.com>
2025-05-15 18:53:11 +0300

    intToRoman

--- part-3.js
+++ part-3.js
@@ -97,5 +97,25 @@ function maxArea(height){
     return res
     
 }
-console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+// console.log(maxArea([1,8,6,2,5,4,8,3,7]));
 
+function intToRoman(num){
+    symList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50], ["XC", 90], ["C", 100], ["CD", 400],["D", 500],["CM", 900],["M", 1000]]
+    let res = ''
+    
+    for(let [sym, val] of symList.reverse()){
+        if ( Math.floor(num / val) > 0){
+            
+            // console.log(num / val);
+            const count = Math.floor(num / val)
+            res += sym.repeat(count)
+            num -= val * count
+            
+        }
+    }
+
+    console.log(res);
+    
+
+}
+intToRoman(1994)
